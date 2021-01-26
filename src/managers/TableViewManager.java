ee50ca2ea3e56598bcbcb3235073f07709e9b579
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-01-26 20:50:06 +0000

    Create TableViewManager.java

---
+++ src/managers/TableViewManager.java
@@ -0,0 +1,52 @@
+package managers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+
+public class TableViewManager<T> {
+	protected final TableView<T> tableView;
+	protected final ObservableList<T> obsList = FXCollections.observableArrayList();
+	
+	public TableViewManager(TableView<T> listView) {
+		this.tableView = listView;
+		this.tableView.setItems(obsList);
+	}
+	
+	public void addItem(T item) {
+		obsList.add(item);
+	}
+	
+	public void removeItem(T item) {
+		obsList.remove(item);
+	}
+	
+	public T getSelectedItem() {
+		return tableView.getSelectionModel().getSelectedItem();
+	}
+	
+	public void addItems(List<T> items) {
+		obsList.addAll(items);
+	}
+	
+	public void setItems(List<T> items) {
+		obsList.setAll(items);
+	}
+	
+	public List<T> getItems() {
+		return obsList.stream().collect(Collectors.toList());
+	}
+	
+	public void replaceItem(T oldItem, T newItem) {
+		int oldItemIndex = obsList.indexOf(oldItem);
+		obsList.remove(oldItemIndex);
+		obsList.add(oldItemIndex, newItem);
+	}
+	
+	public void removeAllItems() {
+		obsList.clear();
+	}
+}
