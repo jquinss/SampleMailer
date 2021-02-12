743d376ffd09495cb9808737813bacacc325845b
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-02-12 20:35:04 +0000

    Create SSLProtocol.java

---
+++ src/enums/SSLProtocol.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum SSLProtocol {
+	SSLv3,
+	TLSv1,
+	TLSv1_1,
+	TLSv1_2;
+	
+	@Override
+	public String toString() {
+		switch (this) {
+		case SSLv3: return "SSLv3";
+		case TLSv1: return "SSLv1";
+		case TLSv1_1: return "TLSv1.1";
+		case TLSv1_2: return "TLSv1.2";
+		default:throw new IllegalArgumentException();
+		}
+	}
+}
