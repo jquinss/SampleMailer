054f3b56df103872a018d00bd06d2593dec246c9
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-02-12 20:36:14 +0000

    Update ScheduledEmailTask.java

--- src/mail/ScheduledEmailTask.java
+++ src/mail/ScheduledEmailTask.java
@@ -3,15 +3,18 @@ package mail;
 import java.time.LocalDateTime;
 import java.util.concurrent.FutureTask;
 
+import util.Observable;
+import util.Observer;
+
 import static java.time.temporal.ChronoUnit.SECONDS;
 
-public class ScheduledEmailTask extends FutureTask<Void> {
-	private final String taskId;
+public class ScheduledEmailTask extends FutureTask<Void> implements Observable {
 	private final LocalDateTime scheduledDateTime;
+	private String taskId;
+	private Observer observer;
 	
-	public ScheduledEmailTask(EmailTask emailTask, String taskId, LocalDateTime scheduledDateTime) {
+	public ScheduledEmailTask(EmailTask emailTask, LocalDateTime scheduledDateTime) {
 		super(emailTask);
-		this.taskId = taskId;
 		this.scheduledDateTime = scheduledDateTime;
 	}
 	
@@ -19,11 +22,32 @@ public class ScheduledEmailTask extends FutureTask<Void> {
 		return taskId;
 	}
 	
-	public LocalDateTime getScheduledDatetime() {
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+	
+	public LocalDateTime getScheduledDateTime() {
 		return scheduledDateTime;
 	}
 	
 	public long getScheduledDateTimeFromNowInSeconds() {
 		return SECONDS.between(LocalDateTime.now(), scheduledDateTime);
+	}
+	
+	public void setObserver(Observer observer) {
+		this.observer = observer;
+	}
+	
+	@Override
+	protected void done() {
+		super.done();
+		if (observer != null) {
+			notifyObserver();
+		}
+		
+	}
+	
+	public void notifyObserver() {
+		observer.update(this);
 	}
 }
