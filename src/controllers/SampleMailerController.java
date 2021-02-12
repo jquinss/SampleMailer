d349acb344d72748ff0bb63267f7a9dca61f4f67
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-02-12 20:34:29 +0000

    Update SampleMailerController.java

--- src/controllers/SampleMailerController.java
+++ src/controllers/SampleMailerController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
@@ -15,6 +18,7 @@ import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.ToggleButton;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.converter.IntegerStringConverter;
 
@@ -22,21 +26,41 @@ import managers.SettingsManager;
 import managers.TableViewManager;
 import managers.TemplateManager;
 import managers.ListViewManager;
+import managers.EmailScheduler;
+import mail.EmailTask;
 import mail.EmailTemplate;
+import mail.MimeMessageBuilder;
+import mail.ScheduledEmailTask;
 import util.DialogBuilder;
+import util.OSChecker;
 import control.DateTimePicker;
 
 import java.util.function.UnaryOperator;
 import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import javax.mail.Header;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 
 public class SampleMailerController {
 
 	@FXML
 	private MenuItem saveTemplatesBtn;
+	
+	@FXML
+	private MenuItem saveTemplatesAndExitBtn;
 
 	@FXML
 	private MenuItem exitAppBtn;
@@ -61,6 +85,7 @@ public class SampleMailerController {
 
 	@FXML
 	private TextField toField;
+	
 	@FXML
 	private TextField ccField;
 
@@ -107,7 +132,7 @@ public class SampleMailerController {
 	private Button removeTemplateBtn;
 
 	@FXML
-	private TableView<?> schedulerTableView;
+	private TableView<ScheduledEmailTask> schedulerTableView;
 
 	@FXML
 	private DateTimePicker dateTimePicker;
@@ -152,6 +177,12 @@ public class SampleMailerController {
 	private ListViewManager<File> attachmentManager;
 	
 	private TableViewManager<Header> headerManager;
+	
+	private EmailScheduler emailScheduler;
+	
+	private final ExecutorService emailTaskExecutor = Executors.newSingleThreadExecutor();
+	
+	private Future<Void> futureSubmittedTask;
 
 	@FXML
 	void addAttachment(ActionEvent event) {
@@ -234,12 +265,20 @@ public class SampleMailerController {
 
 	@FXML
 	void cancelEmail(ActionEvent event) {
-
+		if (futureSubmittedTask != null) {
+			if (futureSubmittedTask.cancel(true)) {
+				System.out.println("Email has been cancelled");
+			}
+		}
 	}
 
 	@FXML
 	void cancelScheduledEmail(ActionEvent event) {
-
+		ScheduledEmailTask scheduledEmailTask = emailScheduler.getSelectedItem();
+		
+		if (scheduledEmailTask != null) {
+			emailScheduler.cancelScheduledEmailTask(scheduledEmailTask);
+		}
 	}
 
 	@FXML
@@ -261,7 +300,8 @@ public class SampleMailerController {
 
 	@FXML
 	void exitApp(ActionEvent event) {
-
+		shutdownExecutors();
+		stage.close();
 	}
 
 	@FXML
@@ -284,8 +324,21 @@ public class SampleMailerController {
 	}
 
 	@FXML
-	void openSettingsDialog(ActionEvent event) {
-
+	void openSettingsDialog(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("..\\view\\SettingsPane.fxml"));
+		Parent parent = fxmlLoader.load();
+		
+		SettingsPaneController settingsPaneController = fxmlLoader.getController();
+		
+		Scene scene = new Scene(parent, 800, 350);
+        Stage stage = new Stage();
+        stage.setResizable(false);
+        
+        settingsPaneController.setStage(stage);
+        
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.showAndWait();
 	}
 
 	@FXML
@@ -317,17 +370,35 @@ public class SampleMailerController {
 
 	@FXML
 	void saveTemplates(ActionEvent event) {
-		templateManager.saveItems();
+		saveTemplatesIfModified();
+	}
+	
+	@FXML
+	void saveTemplatesAndExit(ActionEvent event) {
+		saveTemplatesIfModified();;
+		shutdownExecutors();
+		stage.close();
+	}
+	
+	private void saveTemplatesIfModified() {
+		if (templateManager.isTemplateListModified()) {
+			templateManager.saveItems();
+		}
 	}
 
 	@FXML
 	void scheduleEmail(ActionEvent event) {
-
+		if (dateTimePicker.getDateTimeValue().isAfter(LocalDateTime.now())) {
+			EmailTask emailTask = createEmailTask();
+			ScheduledEmailTask scheduledEmailTask = new ScheduledEmailTask(emailTask, dateTimePicker.getDateTimeValue());
+			emailScheduler.scheduleEmailTask(scheduledEmailTask);
+		}	
 	}
 
 	@FXML
 	void sendEmail(ActionEvent event) {
-
+		EmailTask emailTask = createEmailTask();
+		futureSubmittedTask = emailTaskExecutor.submit(emailTask);
 	}
 
 	@FXML
@@ -353,14 +424,20 @@ public class SampleMailerController {
 
 	private Properties getDefaultSettings() {
 		Properties defaultSettings = new Properties();
+		try {
+			defaultSettings.setProperty("mail.smtp.localhost", InetAddress.getLocalHost().getHostName());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		defaultSettings.setProperty("mail.smtp.port", "25");
 		defaultSettings.setProperty("mail.smtp.connectiontimeout", "-1");
 		defaultSettings.setProperty("mail.smtp.ssl.protocols", "TLSv1.1 TLSv1.2");
 		
 		defaultSettings.setProperty("mail.content.contenttype", "text/html");
-		defaultSettings.setProperty("mail.content.charset", "UTF-8");
+		defaultSettings.setProperty("mail.content.charset", "utf-8");
 		
-		defaultSettings.setProperty("mail.session.debugenabled", "false");
+		defaultSettings.setProperty("mail.debug", "false");
+		defaultSettings.setProperty("mail.debugurl", OSChecker.getOSDataDirectory() + File.separator + "Debug.log");
 
 		return defaultSettings;
 	}
@@ -377,6 +454,7 @@ public class SampleMailerController {
 		templateManager = new TemplateManager(templateListView);
 		attachmentManager = new ListViewManager<File>(attachmentListView);
 		headerManager = new TableViewManager<Header>(headerTableView);
+		emailScheduler = new EmailScheduler(schedulerTableView);
 	}
 
 	private void initializeFields() {
@@ -423,5 +501,81 @@ public class SampleMailerController {
 		};
 
 		return filter;
+	}
+	
+	private MimeMessage createMimeMessage() {
+		Properties settings = SettingsManager.getInstance().getSettings();
+		
+		settings.setProperty("mail.smtp.host", serverNameField.getText().trim());
+		
+		if (!toggleFromField.isDisabled()) {
+			settings.setProperty("mail.smtp.from", mailFromField.getText().trim());
+		}
+		
+		if (!toggleTLSBtn.isDisable()) {
+			settings.setProperty("mail.smtp.starttls.enable", "true");
+		}
+		
+		Session session = Session.getInstance(settings);
+		
+		MimeMessageBuilder mimeMessageBuilder = new MimeMessageBuilder(session);
+		MimeMessage mimeMessage = null;
+		
+		try {
+			mimeMessageBuilder.setFrom(fromField.getText().trim());
+			mimeMessageBuilder.setRecipients(RecipientType.TO, toField.getText().trim());
+			
+			if (!ccField.getText().trim().isEmpty()) {
+				mimeMessageBuilder.setRecipients(RecipientType.CC, ccField.getText().trim());
+			}
+			
+			if (!bccField.getText().trim().isEmpty()) {
+				mimeMessageBuilder.setRecipients(RecipientType.BCC, ccField.getText().trim());
+			}
+			
+			if (!headerManager.getItems().isEmpty()) {
+				mimeMessageBuilder.setHeaders(headerManager.getItems());
+			}
+			
+			if (!subjectField.getText().isEmpty()) {
+				mimeMessageBuilder.setSubject(subjectField.getText());
+			}
+			
+			if (!attachmentManager.getItems().isEmpty()) {
+				mimeMessageBuilder.setMultiPartBody(attachmentManager.getItems(), bodyField.getText(), 
+						settings.getProperty("mail.content.contenttype"), settings.getProperty("mail.content.charset"));
+			}
+			else {
+				mimeMessageBuilder.setBody(bodyField.getText(), settings.getProperty("mail.content.contenttype"), 
+						settings.getProperty("mail.content.charset"));
+			}
+			
+			mimeMessage = mimeMessageBuilder.getMessage();
+		}
+		catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		
+		return mimeMessage;
+	}
+	
+	private EmailTask createEmailTask() {
+		MimeMessage mimeMessage = createMimeMessage();
+		int numEmails = Integer.parseInt(numEmailsField.getText());
+		int delay = Integer.parseInt(numEmailsField.getText());
+		
+		EmailTask emailTask = new EmailTask(mimeMessage, numEmails, delay);
+		
+		return emailTask;
+	}
+	
+	private void shutdownExecutors() {
+		if (!emailTaskExecutor.isShutdown()) {
+			emailTaskExecutor.shutdownNow();
+		}
+		
+		if (!emailScheduler.getScheduledExecutorService().isShutdown()) {
+			emailScheduler.getScheduledExecutorService().shutdownNow();
+		}
 	}
 }
