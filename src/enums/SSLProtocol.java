2206ed2142a1f07a9ff6ca68e529b4ab345f43c0
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-02-14 22:39:08 +0000

    Update SSLProtocol.java

--- src/enums/SSLProtocol.java
+++ src/enums/SSLProtocol.java
@@ -10,7 +10,7 @@ public enum SSLProtocol {
 	public String toString() {
 		switch (this) {
 		case SSLv3: return "SSLv3";
-		case TLSv1: return "SSLv1";
+		case TLSv1: return "TLSv1";
 		case TLSv1_1: return "TLSv1.1";
 		case TLSv1_2: return "TLSv1.2";
 		default:throw new IllegalArgumentException();
