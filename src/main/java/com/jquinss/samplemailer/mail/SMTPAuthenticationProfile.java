e26aa14b6382135df4214594086b61b7f4ff50b9
Joaquin <47039871+jquinss@users.noreply.github.com>
2023-04-09 22:04:12 +0100

    created SMTPAuthenticationProfile.java class

---
+++ src/main/java/com/jquinss/samplemailer/mail/SMTPAuthenticationProfile.java
@@ -0,0 +1,39 @@
+package com.jquinss.samplemailer.mail;
+
+import java.io.Serializable;
+
+public class SMTPAuthenticationProfile implements Serializable {
+    private String emailAddress;
+    private ServerProfile serverProfile;
+    private boolean enabled;
+
+    public SMTPAuthenticationProfile(ServerProfile serverProfile, boolean enabled) {
+        this.serverProfile = serverProfile;
+        this.enabled = enabled;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public ServerProfile getServerProfile() {
+        return serverProfile;
+    }
+
+    public void setServerProfile(ServerProfile serverProfile) {
+        this.serverProfile = serverProfile;
+    }
+
+}
