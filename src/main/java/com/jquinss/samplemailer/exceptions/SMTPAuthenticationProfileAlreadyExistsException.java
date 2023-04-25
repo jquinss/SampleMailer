217f05b4be0a3a2fa5d719ab246c2bd25b6661c9
Joaquin <47039871+jquinss@users.noreply.github.com>
2023-04-25 19:23:07 +0100

    deleted exception

--- src/main/java/com/jquinss/samplemailer/exceptions/SMTPAuthenticationProfileAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.jquinss.samplemailer.exceptions;
-
-public class SMTPAuthenticationProfileAlreadyExistsException extends Exception {
-    public SMTPAuthenticationProfileAlreadyExistsException(String message) {
-        super(message);
-    }
-}
